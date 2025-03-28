a052ffadc2583072624284961c03ef25276bcd79
vasilecampeanu <v.campeanu@topmotive.eu>
2025-03-28 20:28:24 +0200

    feat: Added new autocmd for lsp

--- lua/neonyx/configs/autocmd.lua
+++ lua/neonyx/configs/autocmd.lua
@@ -1,14 +1,31 @@
-
 -- See `:help lua-guide-autocommands`
 -- Highlight when yanking (copying) text
 -- Try it with `yap` in normal mode
 -- See `:help vim.highlight.on_yank()`
 vim.api.nvim_create_autocmd('TextYankPost', {
     desc  = 'Highlight when yanking (copying) text',
-    group = vim.api.nvim_create_augroup('kickstart-highlight-yank', {
+    group = vim.api.nvim_create_augroup('neonyx-highlight-yank', {
         clear = true
     }),
     callback = function()
         vim.highlight.on_yank()
     end
+})
+
+-- Attach LSP keymaps when LSP attaches to a buffer
+vim.api.nvim_create_autocmd('LspAttach', {
+    desc = 'Attach LSP keymaps when LSP attaches to a buffer',
+    callback = function(event)
+        local opts = {
+            buffer = event.buf
+        }
+        vim.keymap.set('n', 'K',    '<cmd>lua vim.lsp.buf.hover()<cr>',           opts)
+        vim.keymap.set('n', 'gd',   '<cmd>lua vim.lsp.buf.definition()<cr>',      opts)
+        vim.keymap.set('n', 'gD',   '<cmd>lua vim.lsp.buf.declaration()<cr>',     opts)
+        vim.keymap.set('n', 'gi',   '<cmd>lua vim.lsp.buf.implementation()<cr>',  opts)
+        vim.keymap.set('n', 'go',   '<cmd>lua vim.lsp.buf.type_definition()<cr>', opts)
+        vim.keymap.set('n', 'gr',   '<cmd>lua vim.lsp.buf.references()<cr>',      opts)
+        vim.keymap.set('n', 'gs',   '<cmd>lua vim.lsp.buf.signature_help()<cr>',  opts)
+        vim.keymap.set('n', '<F2>', '<cmd>lua vim.lsp.buf.rename()<cr>',          opts)
+    end,
 })
--- lua/neonyx/init.lua
+++ lua/neonyx/init.lua
@@ -1,7 +1,7 @@
 require('neonyx.configs')
 
--- See `:help lazy.nvim.txt` or https://github.com/folke/lazy.nvim for more info
 local lazypath = vim.fn.stdpath 'data' .. '/lazy/lazy.nvim'
+
 if not (vim.uv or vim.loop).fs_stat(lazypath) then
     local lazyrepo = 'https://github.com/folke/lazy.nvim.git'
     local out = vim.fn.system { 'git', 'clone', '--filter=blob:none', '--branch=stable', lazyrepo, lazypath }
@@ -9,7 +9,7 @@ if not (vim.uv or vim.loop).fs_stat(lazypath) then
         error('Error cloning lazy.nvim:\n' .. out)
     end
 end ---@diagnostic disable-next-line: undefined-field
+
 vim.opt.rtp:prepend(lazypath)
 
 require('lazy').setup('neonyx.plugins')
-
